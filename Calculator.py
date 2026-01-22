f0d9ebf8d557d48ab1482b567bf76037f5cbd8f8
Stackcraft <Yasminy1804@gmail.com>
2026-01-22 23:33:04 +0000

    First Project

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+operation = input("Choose operation (+, -, *, /): ")
+
+if operation == "+":
+    print("Result:", num1 + num2)
+elif operation == "-":
+    print("Result:", num1 - num2)
+elif operation == "*":
+    print("Result:", num1 * num2)
+elif operation == "/":
+    print("Result:", num1 / num2)
+else:
+    print("Invalid operation")
